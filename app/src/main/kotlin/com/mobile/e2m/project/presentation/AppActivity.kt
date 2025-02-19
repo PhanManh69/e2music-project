9b1a4db07d22eb04e6ac8b150afa5c9b30815013
ManhPK <dc.ctb.manhpk@msb.com.vn>
2025-02-19 17:11:12 +0700

    add notification permission

--- app/src/main/kotlin/com/mobile/e2m/project/presentation/AppActivity.kt
+++ app/src/main/kotlin/com/mobile/e2m/project/presentation/AppActivity.kt
@@ -1,12 +1,28 @@
 package com.mobile.e2m.project.presentation
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import com.mobile.e2m.core.ui.theme.E2MusicTheme
+import com.mobile.e2m.core.ui.R
 
 class AppActivity : ComponentActivity() {
+    private val notificationPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (!isGranted) {
+                showPermissionDeniedMessage()
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,5 +31,25 @@ class AppActivity : ComponentActivity() {
                 RootNavScreen()
             }
         }
+
+        checkAndRequestNotificationPermission()
+    }
+
+    private fun checkAndRequestNotificationPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS)
+                != PackageManager.PERMISSION_GRANTED
+            ) {
+                notificationPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+            }
+        }
+    }
+
+    private fun showPermissionDeniedMessage() {
+        Toast.makeText(this, R.string.permissionDeniedMessage, Toast.LENGTH_LONG).show()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            finishAffinity()
+        }, 2000L)
     }
 }
